71c5360f0e131ef35db8c2a567184daf6d004708
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-09-04 00:18:36 -0600

    Lecture 65: Adding Prisma into GraphQL Subscriptions

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -3,26 +3,38 @@ const Subscription = {
 
     // the comment resolver to listen for comments in a post
     comment: {
-        subscribe(parent, { postId }, { db, pubsub }, info) {
+        subscribe(parent, { postId }, { db, pubsub, prisma }, info) {
 
-            // find the post
-            const post = db.posts.find(post => post.id === postId && post.published);
-
-            // post not found: throw error
-            if (!post) {
-                throw new Error('The post was not found.')
-            }
-
-            // create new channel with the unique post id embedded
-            return pubsub.asyncIterator(`comment ${postId}`);
+            // explanation: the 'where' property allows to subscribe to comments that
+            // match some criteria
+            // such criteria is one where the comment itself has that got created/updated/deleted
+            // is related to the particular post id we passed in as an argument
+            return prisma.subscription.comment({
+                where: {
+                    node: {
+                        post: {
+                            id: postId
+                        }
+                    }
+                }
+            }, info);
 
         }
     },
 
     // the post resolver to listen for new posts: just create the 'post' channel
     post: {
-        subscribe(parent, args, { pubsub }, info) {
-            return pubsub.asyncIterator('post');
+        subscribe(parent, args, { pubsub, prisma }, info) {
+
+            // subscribe to changes in posts that are published
+            return prisma.subscription.post({
+                where: {
+                    node: {
+                        published: true
+                    }
+                }
+            }, info);
+
         }
     }
 

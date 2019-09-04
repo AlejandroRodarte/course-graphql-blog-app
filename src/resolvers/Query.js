1f7c03bc6a39269330ec7e2d4d05bba148583734
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-09-03 20:47:59 -0600

    Lecture 60: Integrating Operation Arguments

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -6,13 +6,55 @@ const Query = {
     // as an object (also allowed besides the string format)
     // we can return promises in the resolver methods
     users(parent, args, { db, prisma }, info) {
-        return prisma.query.users(null, info);
+
+        const opArgs = {};
+
+        // if a query exists
+        if (args.query) {
+
+            // set a 'where' property and check if either the query matches any name or email on the records
+            // all this syntax knowledge is provided by the Prisma API through its auto-generated documentation
+            opArgs.where = {
+                OR: [
+                    {
+                        name_contains: args.query
+                    },
+                    {
+                        email_contains: args.query
+                    }
+                ]
+            }
+
+        }
+
+        // return users according to operation arguments and the selection set the client wants
+        return prisma.query.users(opArgs, info);
+
     },
 
     // the 'posts' query resolver
     // use prisma-binding to query for all psots in the database
     posts(parent, args, { db, prisma }, info) {
-        return prisma.query.posts(null, info);
+
+        const opArgs = {};
+
+        // if query is present: set a where property and check if search string
+        // matches either the post title or body for all records (for more info, check the Prisma API docs!)
+        if (args.query) {
+            opArgs.where = {
+                OR: [
+                    {
+                        title_contains: args.query
+                    },
+                    {
+                        body_contains: args.query
+                    }
+                ]
+            }
+        }
+
+        return prisma.query.posts(opArgs, info);
+
     },
 
     // the 'comments' query resolver: return all comments (part 1)

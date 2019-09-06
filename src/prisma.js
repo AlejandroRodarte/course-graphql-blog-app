c5ff12cd1912cc0001e44e71096c3d6df57c475f
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-09-05 21:28:07 -0600

    Lecture 93: Node.js Production Environment Variables

--- src/prisma.js
+++ src/prisma.js
@@ -12,7 +12,7 @@ import { fragmentReplacements } from './resolvers/index';
 const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
     endpoint: process.env.PRISMA_ENDPOINT,
-    secret: 'my-super-secret-password',
+    secret: process.env.PRISMA_SECRET,
     fragmentReplacements
 });
 

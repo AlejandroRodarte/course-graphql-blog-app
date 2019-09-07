dd3c992f873d14e531893337ee773905e9593bbc
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-09-06 23:53:56 -0600

    Lecture 102: Seeding the Database with Test Data

--- tests/user.test.js
+++ tests/user.test.js
@@ -6,10 +6,59 @@ import 'cross-fetch/polyfill';
 
 import ApolloBoost, { gql } from 'apollo-boost';
 import prisma from '../src/prisma';
+import bcrypt from 'bcryptjs';
 
 // enabling apollo boost to make GraphQL queries
 const client = new ApolloBoost({
     uri: 'http://localhost:4000'
+});
+
+// lifecycle method: runs before each unit test runs
+beforeEach(async () => {
+
+    // wipe out the database (users, posts and comments)
+    await prisma.mutation.deleteManyComments();
+    await prisma.mutation.deleteManyPosts();
+    await prisma.mutation.deleteManyUsers();
+
+    // seed the database with some dummy data (to test login, queries, deletes and updates)
+    // hashing passwors since we are accessing the Prisma API directly
+    const user = await prisma.mutation.createUser({
+        data: {
+            name: 'Alejandro Rodarte',
+            email: 'alex@gmail.com',
+            password: bcrypt.hashSync('jesus')
+        }
+    });
+
+    // first dummy post for the first user
+    await prisma.mutation.createPost({
+        data: {
+            title: 'Post 1 by Alejandro.',
+            body: 'This is my first post.',
+            published: true,
+            author: {
+                connect: {
+                    id: user.id
+                }
+            }
+        }
+    });
+
+    // second dummy post for the first user
+    await prisma.mutation.createPost({
+        data: {
+            title: 'Post 2 by Alejandro.',
+            body: 'This is my second post.',
+            published: false,
+            author: {
+                connect: {
+                    id: user.id
+                }
+            }
+        }
+    });
+
 });
 
 // test: create a user in the database
